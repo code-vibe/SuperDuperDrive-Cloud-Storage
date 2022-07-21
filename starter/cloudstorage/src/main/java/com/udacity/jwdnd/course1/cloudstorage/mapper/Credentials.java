dc2e90003d6723a5bb427303b1461f3526320517
code-vibe <mrsamoladapo@gmail.com>
2022-07-21 22:26:33 +0100

    map credentials model classes with database data

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/Credentials.java
@@ -0,0 +1,23 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import org.apache.ibatis.annotations.*;
+
+public interface Credentials {
+    @Select("SELECT * FROM CREDENTIALS WHERE userid = #{UserId}")
+    Credentials[] getCredentials(String credentials);
+
+    @Select("SELECT * FROM CREDENTIALS WHERE credentialsid = #{credentialsId}")
+    Credentials getCredentialsById(Integer credentialsId);
+
+    @Insert("INSERT INTO CREDENTIALS (url, username, key, password, userid) " +
+            "VALUES(#{url}, #{userName}, #{key}, #{password}, #{userid})")
+    @Options(useGeneratedKeys = true, keyProperty = "credentialid")
+    int insert(Credentials credentials);
+
+    @Delete("DELETE FROM CREDENTIALS WHERE credentialsid = #{credentialsId}")
+    void deleteCredentials(Integer credentialsId);
+
+    @Update("UPDATE CREDENTIALS SET url = #{url}, key = #{key}, password = #{password}, username = #{newUserName} WHERE credentialsid = #{credentialsId}")
+    void updateCredentials(Integer credentialsId, String newUserName, String url, String key, String password);
+
+}

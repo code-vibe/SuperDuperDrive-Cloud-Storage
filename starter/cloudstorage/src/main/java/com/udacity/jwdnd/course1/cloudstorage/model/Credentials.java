a84620ef3d1b8f8e00da680ef000f37d04dbccb8
code-vibe <mrsamoladapo@gmail.com>
2022-07-21 13:11:35 +0100

    POJO to match data in database

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Credentials.java
@@ -0,0 +1,78 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+public class Credentials {
+    private Integer credentialsId;
+    private String url;
+    private String username;
+    private String key;
+    private String password;
+    private String foreignKey;
+    private Integer userId;
+
+    public Credentials(Integer credentialsId, String url, String username,
+                       String key, String password, String foreignKey,Integer userId ) {
+        this.credentialsId = credentialsId;
+        this.url = url;
+        this.username = username;
+        this.key = key;
+        this.password = password;
+        this.foreignKey = foreignKey;
+        this.userId = userId;
+    }
+
+    public Integer getCredentialsId() {
+        return credentialsId;
+    }
+
+    public void setCredentialsId(Integer credentialsId) {
+        this.credentialsId = credentialsId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getForeignKey() {
+        return foreignKey;
+    }
+
+    public void setForeignKey(String foreignKey) {
+        this.foreignKey = foreignKey;
+    }
+}

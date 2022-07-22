33bb99c494fcaa4bf3615fc01b0e7efead7bd874
code-vibe <mrsamoladapo@gmail.com>
2022-07-22 07:39:05 +0100

    managed user access with SecurityConfig

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.udacity.jwdnd.course1.cloudstorage.config;
+
+import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+
+private AuthenticationService authenticationService;
+
+public SecurityConfig(AuthenticationService authenticationService) {
+    this.authenticationService = authenticationService;
+}
+@Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+    auth.authenticationProvider(this.authenticationService);
+}
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/signup", "/js", "/css/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("/home", true)
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+
+}
